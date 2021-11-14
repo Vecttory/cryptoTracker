e7163270c1a9640a330347afa0b1f49be934b21a
Alberto Ola <alberto.ola.dev@gmail.com>
2021-11-14 02:15:32 -0600

    styling flatlist items based on OS

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { View, Text, Pressable, ActivityIndicator, StyleSheet, FlatList } from 'react-native';
 import Http from 'cryptoTracker/src/libs/http';
 import CoinsItem from "./CoinsItem";
+import Colors from "../../res/colors"
 
 class CoinsScreen extends Component {
 
@@ -48,7 +49,7 @@ class CoinsScreen extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#fff"
+        backgroundColor: Colors.charade
     },
     titleText: {
         color: "#fff",
